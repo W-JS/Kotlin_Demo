160e596e9efda277c394dc6baf423f8efe8109ce
wanjinsheng <wanjinshengwjs@163.com>
2023-07-02 21:46:12 +0800

    Kotlin+MVVM模式下，TargetActivity新增一个导航项-Point

--- app/src/main/java/com/example/demo/TargetActivity.kt
+++ app/src/main/java/com/example/demo/TargetActivity.kt
@@ -32,7 +32,7 @@ class TargetActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_point
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
